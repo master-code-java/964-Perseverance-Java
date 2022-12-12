c02cad92c2ce8ab858817d470e9eb05c6c926957
Thiago <83519346+master-code-java@users.noreply.github.com>
2022-12-12 21:10:52 +0000

    adding greather than signal

--- CalculaMédia.java
+++ CalculaMédia.java
@@ -5,7 +5,7 @@ public class CalculaMédia {
         float nota1 = Float.parseFloat(args[0]);
         float nota2 = Float.parseFloat(args[1]);
 
-        String resultado = (nota1 + nota2) > NOTA_DE_CORTE ? "Aprovado" : "Reprovado";
+        String resultado = (nota1 + nota2) >= NOTA_DE_CORTE ? "Aprovado" : "Reprovado";
 
         System.out.println("O Aluno está: " + resultado);
 
