26e9ef4cf5251967dbd8ea107235805ff0fa2078
Thiago <83519346+master-code-java@users.noreply.github.com>
2022-12-12 21:10:09 +0000

    Add files via upload

---
+++ CalculaMédia.java
@@ -0,0 +1,13 @@
+public class CalculaMédia {
+    private static final float NOTA_DE_CORTE = 7.0f;
+
+    public static void main(String[] args) {
+        float nota1 = Float.parseFloat(args[0]);
+        float nota2 = Float.parseFloat(args[1]);
+
+        String resultado = (nota1 + nota2) > NOTA_DE_CORTE ? "Aprovado" : "Reprovado";
+
+        System.out.println("O Aluno está: " + resultado);
+
+    }
+}
